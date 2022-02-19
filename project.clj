7c65d3f3c89be78fe81c7b556718aa71e17be3cf
Jack Ducat <jack@blueridgeglobal.com.au>
2022-02-19 12:03:57 +1100

    Update Project file for Heroku

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject spelling-bee "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :url "https://class-spell-bee.herokuapp.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
@@ -10,7 +10,8 @@
                  [org.clojure/clojurescript "1.10.764"]
                  [cljs-http "0.1.46"]
                  [reagent "0.10.0"]
-                 [re-frame "0.12.0"]]
+                 [re-frame "0.12.0"]
+                 [environ "1.1.0"]]
 
   :source-paths ["src" "test"]
 
@@ -19,7 +20,9 @@
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "spelling-bee.test-runner"]}
 
+  :plugins [[environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "class-spelling-bee.jar"
   :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.5"]
-                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]}}
-  
-  :uberjar-name "class-spelling-bee.jar")
+                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]}
+             :production {:env {:production true}}})
